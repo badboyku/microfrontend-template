09add1de8db3b7f9a28944f12ab75c217095ba5a
Joey Atkinson <badboyKU@gmail.com>
2021-09-12 22:24:47 -0700

    Fix sass support

--- src/components/HelloWorld/HelloWorld.tsx
+++ src/components/HelloWorld/HelloWorld.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
-import logoUrl, {ReactComponent as Logo} from './logo.svg';
 import './styles.css';
 
 type Props = {};
 
 const HelloWorld = (_props: Props): JSX.Element => {
-  return (
-    <div>
-      <div className="hello-world">Hello World!!!</div>
-      <img src={logoUrl} className="logo" alt="logo" />
-      <Logo width={40} />
-    </div>
-  );
+  return <div className="hello-world">Hello World!!!</div>;
 };
 
 export default HelloWorld;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import HelloWorld from '../../components/HelloWorld';
+import logoUrl, {ReactComponent as Logo} from './logo.svg';
+import './style.scss';
+import './styles.css';
 
 type Props = {};
 
 const Home = (_props: Props): JSX.Element => {
   return (
     <div>
+      <h1>Home</h1>
       <HelloWorld />
+      <img src={logoUrl} className="logo" alt="logo" />
+      <Logo width={40} />
     </div>
   );
 };
