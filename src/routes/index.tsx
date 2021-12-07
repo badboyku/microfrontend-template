246dcc399fcd5667f3d7b8667112e025bd1834b8
Joey Atkinson <badboyKU@gmail.com>
2021-12-06 22:04:15 -0800

    Update packages: @testing-library/jest-dom, core-js, webpack

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Home as HomePage } from '../pages';
+import { Home } from '../pages';
 
 type Props = {};
 
@@ -8,7 +8,7 @@ const AppRoutes: React.FC<Props> = (_props: Props) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
